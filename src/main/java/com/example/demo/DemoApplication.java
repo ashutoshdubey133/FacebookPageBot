ddbdddf8585c05b15db4554cc2c57c5b1feca950
Ashutosh Dubey <ashutoshdubey133@gmail.com>
2024-02-09 00:48:05 +0530

    Added Method to postImage

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,10 +18,37 @@ public class DemoApplication {
 		String message = "Hi From Java";
 
 		//Add Your Token Here
-
 		String token = "";
-		String url = "https://graph.facebook.com/v19.0/cricketvideosindian/feed";
+		
+		String post_url = "https://graph.facebook.com/v19.0/cricketvideosindian/feed";
 
+		//String response = postText(message, token, rt, url);
+
+//		System.out.println(response);
+
+		String imagePostURL = "https://graph.facebook.com/v19.0/cricketvideosindian/photos";
+
+		String imageURL = "https://static.toiimg.com/thumb/msid-107389941,width-400,resizemode-4/107389941.jpg";
+
+//		String res = postImage(imageURL, token, rt, imagePostURL);
+
+	}
+
+	private static String postImage(String imageURL, String token, RestTemplate rt, String imagePostURL) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		String json = "{\n" +
+				"           \"url\":\"" + imageURL + "\",\n" +
+				"           \"access_token\":\""+ token +"\"\n" +
+				"         }";
+
+		HttpEntity<String> request = new HttpEntity<>(json,headers);
+
+		return rt.postForObject(imagePostURL, request, String.class);
+	}
+
+	private static String postText(String message, String token, RestTemplate rt, String url) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -32,9 +59,7 @@ public class DemoApplication {
 
 		HttpEntity<String> request = new HttpEntity<>(json,headers);
 
-		String response = rt.postForObject(url, request, String.class);
-
-		System.out.println(response);
+		return rt.postForObject(url, request, String.class);
 	}
 
 }
